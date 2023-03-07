05a123d17bbd42c68dbb30879869499fdaece6a4
Santiago Otalvaro Ospina <santiago.otalvaroo@udea.edu.co>
2023-03-06 19:24:11 -0500

    console app added

---
+++ PokeDexBack.ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PokeDexBack.ConsoleApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var serviceProvider = new ServiceCollection()
+                .BuildServiceProvider();
+
+            var builder = new ConfigurationBuilder();
+            builder.SetBasePath(Directory.GetCurrentDirectory())
+                   .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+            IConfiguration config = builder.Build();
+
+            var conn = config.GetSection("ApiSettings").GetSection("UrlString").Value;
+        }
+    }
+}
